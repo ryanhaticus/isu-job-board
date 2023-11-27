b35a82af76e5c0a6c2b206ebec5d995ec5c2b685
Ryan Huellen <ryan@huellenconsulting.com>
2023-11-26 23:02:30 -0600

    refactor: directories

--- src/lib/models/user.ts
+++ src/lib/models/user.ts
@@ -1,15 +1,16 @@
 import mongoose, { Schema, model, Model } from 'mongoose';
 
-interface IUser {
+type IUser = {
   name: string;
   email: string;
-}
+};
 
 const userSchema = new Schema<IUser>({
   name: { type: String, required: true },
-  email: { type: String, required: true }
+  email: { type: String, required: true },
 });
 
-const User: Model<IUser> = mongoose.models.User || model<IUser>('User', userSchema);
+const User: Model<IUser> =
+  mongoose.models.User || model<IUser>('User', userSchema);
 
 export default User;
