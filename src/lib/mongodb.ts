4d3712fd6c8a1526b91fa01abd23835c38db45f0
Ryan Huellen <ryan@huellenconsulting.com>
2023-11-26 22:59:59 -0600

    fix: concise mongoose with automatic pooling

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,26 +1,5 @@
-import mongoose from 'mongoose';
+import { createConnection } from 'mongoose';
 
-const mongoDBUrl = 'mongodb://mongo:Bfb13hfCCFDGfB2h4cEHfgBf2GD6HDH6@roundhouse.proxy.rlwy.net:37624';
-
-let cached = global.globalMongoose;
-
-if (!cached) {
-  cached = global.globalMongoose = { conn: null, promise: null };
-}
-
-async function connectToDatabase() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(mongoDBUrl).then((mongoose) => {
-      return mongoose;
-    });
-  }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
-
-export default connectToDatabase;
+export const db = createConnection(process.env.MONGO_URL!, {
+  maxPoolSize: 10,
+});
